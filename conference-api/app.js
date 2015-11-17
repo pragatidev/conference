c19b84b0182791bd18c1e4756ceddc0ef1522ae3
pragati <pragati@pragati-HP-ENVY-TS-15-Notebook-PC>
2015-11-16 23:20:45 -0800

    Adding

--- conference-api/app.js
+++ conference-api/app.js
@@ -2,9 +2,7 @@
  * Module dependencies.
  */
 
-var express = require('express'), routes = require('./routes'), 
-	user = require('./routes/user'), speakers = require('./routes/speakers'), 
-	http = require('http'), path = require('path');
+var express = require('express'), routes = require('./routes'), user = require('./routes/user'), speakers = require('./routes/speakers'), http = require('http'), path = require('path');
 
 var app = express();
 
@@ -25,11 +23,11 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
-/ connect to our database
+// connect to our database
 // you can use your own MongoDB installation at: mongodb://127.0.0.1/
 // databasename
-mongoose.connect('mongodb://username:password@kahana.mongohq.
-com:10073/node-api');
+mongoose
+		.connect('mongodb://username:password@kahana.mongohq.com:10073/node-api');
 
 app.get('/', routes.index);
 app.get('/users', user.list);
